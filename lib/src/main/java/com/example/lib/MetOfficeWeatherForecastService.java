b976311b3904dcb358bab79756bb107921f14c77
Jon Wood <jonathan.m.wood95@gmail.com>
2021-03-25 09:40:17 +0000

    Added a test comment

--- lib/src/main/java/com/example/lib/MetOfficeWeatherForecastService.java
+++ lib/src/main/java/com/example/lib/MetOfficeWeatherForecastService.java
@@ -10,7 +10,7 @@ import java.util.ArrayList;
 
 public class MetOfficeWeatherForecastService implements WeatherForecastService {
     MetOfficeWeatherForecastService() {
-
+        // Test comment
     }
 
 
