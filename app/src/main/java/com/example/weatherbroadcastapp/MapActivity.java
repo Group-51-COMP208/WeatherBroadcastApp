9b2330a6d43c5c901763938c2ec401ae2cd94e32
Jon Wood <jonathan.m.wood95@gmail.com>
2021-04-17 20:59:13 +0100

    Created custom view for map screen. Made function to position elements on map by longitude/latitude. Drawing a little sun icon over Liverpool, which appears to be working, although the icon is virtually invisible against the background, which is a design issue.

--- app/src/main/java/com/example/weatherbroadcastapp/MapActivity.java
+++ app/src/main/java/com/example/weatherbroadcastapp/MapActivity.java
@@ -3,6 +3,9 @@ package com.example.weatherbroadcastapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.FrameLayout;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MapActivity extends AppCompatActivity {
 
@@ -10,5 +13,15 @@ public class MapActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map);
+//        FrameLayout frameLayout = findViewById(R.id.frameLayout_main);
+//        ImageView mapView = findViewById(R.id.imageView_map);
+//
+//        TextView textView1 = new TextView(this);
+//        textView1.setLayoutParams(new FrameLayout.LayoutParams(FrameLayout.LayoutParams.WRAP_CONTENT,
+//                FrameLayout.LayoutParams.WRAP_CONTENT));
+//        textView1.setText("programmatically created TextView1");
+//        textView1.setBackgroundColor(0xff66ff66); // hex color 0xAARRGGBB
+//        textView1.setPadding(20, 20, 20, 20);// in pixels (left, top, right, bottom)
+//        frameLayout.addView(textView1);
     }
 }
