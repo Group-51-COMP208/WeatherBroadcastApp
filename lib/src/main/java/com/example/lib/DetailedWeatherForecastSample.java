58fb4fa24699e444f3d3fd10e66d28ee3b10037b
Jon Wood <jonathan.m.wood95@gmail.com>
2021-04-24 13:23:48 +0100

    Added documentation

--- lib/src/main/java/com/example/lib/DetailedWeatherForecastSample.java
+++ lib/src/main/java/com/example/lib/DetailedWeatherForecastSample.java
@@ -3,6 +3,7 @@ package com.example.lib;
 import java.text.SimpleDateFormat;
 
 public class DetailedWeatherForecastSample extends WeatherForecastSample {
+    // 0 degrees is North and clockwise is the positive direction
     public float windDirection_degrees;
     public float windSpeed_mph;
     public float temperature_celsius;
